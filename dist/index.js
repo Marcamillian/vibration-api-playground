add46b1d13b9e7089bb29da432d58f827e672771
Marc Vousden <marcamillian@gmail.com>
2024-03-19 13:33:58 -0400

    feature: Investigate < 1000ms vibrations
    It seems that I `navigator.vibrate()` commands for < 1000ms don't seem to work.
    
    Trying `navigator.vibrate(Infinity) with a stop afterwards to see if we can get a vibration < 1000 ms

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
-console.log("something")
+let depressedStart; 
+
+let vibrationLengthDisplayElement = document.querySelector(".vibrate-display")
 
 var buttonClicked = (event)=>{
   console.log("button Clicked")
 
-  const vibrationRate = document.querySelector(".vibration-rate").value
+  const vibrationRate = document.querySelector(".vibration-length").value
 
   if('vibrate' in navigator){
     const vibrationSucess = navigator.vibrate(vibrationRate)
@@ -12,6 +14,21 @@ var buttonClicked = (event)=>{
 
 }
 
-document.querySelector(".vibrate-1").addEventListener("click", buttonClicked)
+var vibrateStart = ()=>{
+  depressedStart = new Date()
+  navigator.vibrate(Infinity)
+  console.log(`vibrate start: ${depressedStart}`)
+  
+}
 
+var vibrateEnd = ()=>{
+  const depressedMs = new Date() - depressedStart;
+  navigator.vibrate(0)
+  vibrationLengthDisplayElement.innerHTML = `last vibration ${depressedMs}ms long`
+  console.log(`vibration ended after ${depressedMs}ms`)
+} 
+
+document.querySelector(".vibrate-1").addEventListener("click", buttonClicked)
 document.querySelector(".vibrate-2").addEventListener("touchstart", buttonClicked)
+document.querySelector(".vibrate-3").addEventListener("touchstart", vibrateStart)
+document.querySelector(".vibrate-3").addEventListener("touchend", vibrateEnd)
