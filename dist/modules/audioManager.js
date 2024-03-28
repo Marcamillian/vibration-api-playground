3a7e81a8656c60f3916121fe6eeec432a50c8b19
Marc Vousden <marcamillian@gmail.com>
2024-03-27 21:31:53 -0400

    feature: Improved the AudioManager
    - before the tone was stopped with `.stop()`
      - this meant that a new oscillator was needed each time
    - now calls `.suspend()` on the the `AudioContext`
      - this means that we don't need to create an oscillator each time

--- dist/modules/audioManager.js
+++ dist/modules/audioManager.js
@@ -2,13 +2,6 @@ export default class AudioManager{
   
   constructor(){
     this.audioCtx = new AudioContext();
-    this.oscillator;
-  }
-
-  /** 
-   * Plays a sound on the audio context
-   */
-  signalOn = ()=>{
     this.oscillator = this.audioCtx.createOscillator();
 
     this.oscillator.type = "square";
@@ -16,14 +9,22 @@ export default class AudioManager{
     this.oscillator.connect(this.audioCtx.destination)
     this.oscillator.start()
 
+    this.audioCtx.suspend()
+  }
+
+  /** 
+   * Plays a sound on the audio context
+   * @return a promise that resolves when the audio context is resumed
+   */
+  signalOn = ()=>{
+    return this.audioCtx.resume()
   }
 
   /**
    * Stops the sound playing on the audio context
+   * @return a promise that resolves when the audio context is suspended
    */
   signalOff = ()=>{
-    if(!this.oscillator) return 
-    this.oscillator.stop()
-    this.oscillator = null
+    return this.audioCtx.suspend()
   }
 }
