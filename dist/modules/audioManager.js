3e23f26febfe0142a2098a3a717b0889bc3651df
Marc Vousden <marcamillian@gmail.com>
2024-04-02 14:42:41 -0400

    Revert "feature: Improved the AudioManager"
    This reverts commit 3a7e81a8656c60f3916121fe6eeec432a50c8b19.

--- dist/modules/audioManager.js
+++ dist/modules/audioManager.js
@@ -2,6 +2,13 @@ export default class AudioManager{
   
   constructor(){
     this.audioCtx = new AudioContext();
+    this.oscillator;
+  }
+
+  /** 
+   * Plays a sound on the audio context
+   */
+  signalOn = ()=>{
     this.oscillator = this.audioCtx.createOscillator();
 
     this.oscillator.type = "square";
@@ -9,22 +16,14 @@ export default class AudioManager{
     this.oscillator.connect(this.audioCtx.destination)
     this.oscillator.start()
 
-    this.audioCtx.suspend()
-  }
-
-  /** 
-   * Plays a sound on the audio context
-   * @return a promise that resolves when the audio context is resumed
-   */
-  signalOn = ()=>{
-    return this.audioCtx.resume()
   }
 
   /**
    * Stops the sound playing on the audio context
-   * @return a promise that resolves when the audio context is suspended
    */
   signalOff = ()=>{
-    return this.audioCtx.suspend()
+    if(!this.oscillator) return 
+    this.oscillator.stop()
+    this.oscillator = null
   }
 }
